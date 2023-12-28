c76a252b60676237b3d20bef3ec3f3b2e11602d6
thminggg <tsehongming21@gmail.com>
2023-12-27 17:44:31 -0800

    chore: add building images and temp .env to track images
    .env tracks number of images of condos and townhouses in assets, which
    will be removed once the database is setup with automation script prepare the
    data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
     screens: {
       sm: "640px",
@@ -13,5 +16,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
