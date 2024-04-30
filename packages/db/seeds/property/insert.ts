07dfb67f446b0759d9d38e4a362f2f3bd8a85191
thminggg <tsehongming21@gmail.com>
2024-04-30 02:17:58 -0700

    dev: updagte insert script of property

--- packages/db/seeds/property/insert.ts
+++ packages/db/seeds/property/insert.ts
@@ -1,12 +1,20 @@
 import csv from "csv-parser";
 import fs from "fs";
+import { Property } from "../../@types";
 import postgresSQL from "../../db";
 import { getRandomCity } from "../utils/randomCity";
 import { getRandomHousingType } from "../utils/randomHouseType";
+import { getRandomIds } from "../utils/randomIds";
+import { getRandomItemFromArr } from "../utils/randomItem";
 import { getRandomPostalCode } from "../utils/randomPostal";
 import { getRandomTimestamp } from "../utils/randomTimestamp";
 
-function processData(data, province, country) {
+const processData = (
+  data: Property,
+  province: string,
+  country: string,
+  brokerIds: string[]
+) => {
   const {
     name,
     address,
@@ -32,7 +40,8 @@ function processData(data, province, country) {
       num_of_view,
       listed_timestamp,
       size,
-      type
+      type,
+      broker_id
     )
     VALUES (
         gen_random_uuid(),
@@ -49,12 +58,14 @@ function processData(data, province, country) {
         ${Math.floor(getRandomTimestamp() / 1000)},
         ${size},
         ${getRandomHousingType()}
+        ${getRandomItemFromArr(brokerIds)}
       );
   `;
-}
+};
 
 const insertData = () => {
   const [province, country = "Canada"] = process.argv.slice(2);
+  const brokerIds = getRandomIds();
   const stream = fs
     .createReadStream(`${province.toLocaleLowerCase()}.csv`)
     .pipe(csv());
@@ -62,11 +73,16 @@ const insertData = () => {
 
   stream.on("data", (data) => {
     console.log(data, province, country);
-    insertionPromises.push(processData(data, province, country.toUpperCase()));
+    insertionPromises.push(
+      processData(data, province, country.toUpperCase(), brokerIds)
+    );
   });
 
   stream.on("end", async () => {
-    await Promise.all(insertionPromises);
+    await Promise.all(insertionPromises).catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
     console.log("Data Inserted!!");
     process.exit(); // Exit the process after all data is inserted
   });
