980da4cbdc8317ca0d2daa8690e7b3e105191da0
thminggg <tsehongming21@gmail.com>
2024-06-15 01:14:46 -0700

    chore: update dev bash script
    Compile and run backend service only for development

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,12 +3,29 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+backend_only=false
+
+# Get flag
+while getopts "b" opt; do
+  case $opt in
+    b) backend_only=true;;
+    \?) echo "Invalid option: -$OPTARG";;
+  esac
+done
+
 # Check if docker is running
 if ! docker info > /dev/null 2>&1; then
   echo -e "${RED}error${NC} This script uses docker, and it isn't running - please start docker and try again!"
   exit 1
 fi
 
+# Compile
+npx lerna run compile
+
 # Run dev
-npx lerna run compile 
-npx lerna run dev
+if [ "$backend_only" = false ]; then
+  npx lerna run dev
+else
+  npx lerna run dev:be
+fi
+
