8bcef7e67a0001cf68101d64f5b20a0f4bab95c4
thminggg <tsehongming21@gmail.com>
2024-06-17 22:46:03 -0700

    dev: add utils function

--- packages/frontend/src/utils/utils.ts
+++ packages/frontend/src/utils/utils.ts
@@ -25,3 +25,37 @@ export const clamp = (min: number = 0, max: number = Infinity) => {
 export const capFirst = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+/**
+ * Return number of day between epoch and today
+ * @param {number} epoch
+ * @returns
+ */
+export const daysBetween = (epoch: number) => {
+  // Get today's date
+  const today = new Date();
+
+  // Convert epoch to a Date object
+  const epochDate = new Date(epoch * 1000);
+
+  // Calculate the difference in time
+  const diffTime = today.getTime() - epochDate.getTime();
+
+  // Calculate the difference in days
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+  return diffDays;
+};
+
+/**
+ * Get a random number from 1 to {max}
+ * @param {number} max Max number to be returned
+ * @returns
+ */
+export const getRandomNumber = (max: number) => {
+  // Ensure max is at least 1
+  max = Math.max(1, max);
+
+  // Generate a random number between 1 and max (both inclusive)
+  return Math.floor(Math.random() * max) + 1;
+};
