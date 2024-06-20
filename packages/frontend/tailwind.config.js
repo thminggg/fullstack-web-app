e79bdb834bcb358769b982e10a4a3825bf6e57d4
thminggg <tsehongming21@gmail.com>
2024-06-19 22:56:49 -0700

    dev: add sass to frontend

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -5,15 +5,6 @@ module.exports = {
     "../../node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-      // custom breakpoint
-      "3xl": "2560px",
-    },
     extend: {},
   },
   plugins: [require("flowbite/plugin")],
