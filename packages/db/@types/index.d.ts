7ec3d6a28f768d033d8e5959eb468e9b5f616fcd
thminggg <tsehongming21@gmail.com>
2024-07-10 00:46:59 -0700

    dev: add users SQL

--- packages/db/@types/index.d.ts
+++ packages/db/@types/index.d.ts
@@ -1,6 +1,7 @@
 export { default as brokerCompanies } from "../sql/brokerCompanies";
 export { default as brokers } from "../sql/brokers";
 export { default as properties } from "../sql/properties";
+export { default as users } from "../sql/users";
 export * from "./broker";
 export * from "./brokerCompany";
 export * from "./property";
